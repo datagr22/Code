c1f390c5b4e9308854a846926a56db06e1eff6ec
haakondm <74726804+haakondm@users.noreply.github.com>
2020-11-24 18:15:23 +0100

    Add files via upload

---
+++ ESP32_socket_prosjekt_servo.ino
@@ -0,0 +1,101 @@
+#include <Servo_ESP32.h>
+#include <analogWrite.h>
+
+//Selve bilen
+
+const int servoPin = 13;
+
+int TurnState;
+
+
+Servo_ESP32 myServo;
+
+//Eksempelkoden
+
+#include <WiFi.h>//Imports the needed WiFi libraries
+#include <WiFiMulti.h> //We need a second one for the ESP32 (these are included when you have the ESP32 libraries)
+
+#include <SocketIoClient.h> //Import the Socket.io library, this also imports all the websockets
+
+WiFiMulti WiFiMulti; //Declare an instane of the WiFiMulti library
+SocketIoClient webSocket; //Decalre an instance of the Socket.io library
+
+void ControlServo(Servo_ESP32 myServo);
+
+
+void event(const char * payload, size_t length) { //Default event, what happens when you connect
+  Serial.printf("got message: %s\n", payload);
+}
+
+void changeTurnState(const char * TurnStateData, size_t length) {
+  Serial.printf("Turn State: %s\n", TurnStateData);
+  Serial.println(TurnStateData);
+
+  //Data conversion
+  String dataString(TurnStateData);
+  TurnState = dataString.toInt();
+
+
+  Serial.print("This is the Turn state in INT: ");
+  Serial.println(TurnState);
+
+  
+  ControlServo(TurnState);
+}
+
+void setup() {
+    //SELVE BILEN
+
+    myServo.attach(servoPin);
+    
+    //EKSEMPELKODE
+    Serial.begin(9600); //Start the serial monitor
+
+    
+    Serial.setDebugOutput(true); //Set debug to true (during ESP32 booting)
+
+    Serial.println();
+    Serial.println();
+    Serial.println();
+
+      for(uint8_t t = 4; t > 0; t--) { //More debugging
+          Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+          Serial.flush();
+          delay(1000);
+      }
+
+    WiFiMulti.addAP("Mathisen", "sommerfugl94"); //Add a WiFi hotspot (addAP = add AccessPoint) (put your own network name and password in here)
+
+    while(WiFiMulti.run() != WL_CONNECTED) { //Here we wait for a successfull WiFi connection untill we do anything else
+      Serial.println("Not connected to wifi...");
+        delay(100);
+    }
+
+    Serial.println("Connected to WiFi successfully!"); //When we have connected to a WiFi hotspot
+
+    //Here we declare all the different events the ESP32 should react to if the server tells it to.
+    //a socket.emit("identifier", data) with any of the identifieres as defined below will make the socket call the functions in the arguments below
+    webSocket.on("clientConnected", event); //For example, the socket.io server on node.js calls client.emit("clientConnected", ID, IP) Then this ESP32 will react with calling the event functio
+    webSocket.on("TurnStateChange", changeTurnState);
+    
+
+    webSocket.begin("10.0.0.24", 2520); //This starts the connection to the server with the ip-address/domainname and a port (unencrypted)
+}
+
+//Turn the car left or right (turns with the frontwheels)
+void ControlServo(int TurnState) {
+
+  if(TurnState == 1) {
+    myServo.write(0);
+  }
+  else if(TurnState == -1){
+    myServo.write(100);
+  }
+  else if(TurnState == 0){
+    myServo.write(60);
+  }
+}
+
+void loop() {
+  webSocket.loop();
+}
